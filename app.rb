f20bfe5cd6fbac9e9cab4923479dd3113a050a61
olanike olatunji <olanikegloria2020@gmail.com>
2023-09-12 09:08:48 +0100

    created app.rb file and initialize all its methods

---
+++ app.rb
@@ -0,0 +1,55 @@
+class App
+  def initialize
+    puts 'Catalog og my things ☺️'
+    @books = []
+    @music_albums = []
+    @games = []
+    @genres = []
+    @labels = []
+    @authors = []
+  end
+
+  def option_methods(option)
+    case option
+    when 1..6
+      list(option)
+    when 7..9
+      add(option)
+    else
+      puts 'Thank you for using this app!'
+      save_and_exit
+    end
+  end
+
+  def list(option)
+    case option
+    when 1
+      puts 'list_all_books'
+    when 2
+      puts 'list_all_music_albums'
+    when 3
+      puts 'list_of_games'
+    when 4
+      puts 'list_all_genres'
+    when 5
+      puts 'list_all_labels'
+    else
+      puts 'list_all_authors'
+    end
+  end
+
+  def add(option)
+    case option
+    when 7
+      puts 'add_book'
+    when 8
+      puts 'add_music_album'
+    else
+      puts 'add_game'
+    end
+  end
+
+  def save_and_exit
+    exit
+  end
+end
