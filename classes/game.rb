f9b3eac1dc89d26e5898e4f0f4ba53bab56c90d1
yemidada <yemidada@gmail.com>
2023-09-12 10:35:53 +0100

    Fix linter error

--- classes/game.rb
+++ classes/game.rb
@@ -10,12 +10,6 @@ class Game < Item
     @last_played_at = params[:last_played_at]
   end
 
-  def create_date(date_string)
-    return nil if date_string.nil?
-
-    Date.parse(date_string.to_s)
-  end
-
   def can_be_archived?
     @archived
   end
