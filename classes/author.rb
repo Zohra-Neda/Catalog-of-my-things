737d75574c130dfb0bf2a6c5163292512f6dd247
yemidada <yemidada@gmail.com>
2023-09-12 15:39:40 +0100

    Update author and game files

--- classes/author.rb
+++ classes/author.rb
@@ -2,18 +2,23 @@ require_relative 'item'
 
 class Author < Item
   attr_accessor :id, :first_name, :last_name
-  attr_reader :items
 
   def initialize(params)
     super(params)
 
-    @id = params['id']
+    @id = params['id'] || rand(1..10_000)
     @first_name = params['first_name']
     @last_name = params['last_name']
-    @items = []
   end
 
-  def add_item(item)
-    @items << item
-  end
 end
+
+arr = {
+    'id' => 122,
+    'first_name' => 'Yemi',
+    'last_name' => 'Dada'
+  }
+
+author = Author.new(arr)
+
+author.add_item
