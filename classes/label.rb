2d060e41b2837705996eaac5c8d83e881a9eceb8
zohra-neda <zohra.neda@gmail.com>
2023-09-12 16:02:22 +0430

    Create label class and define its all properties in the constructor method

---
+++ classes/label.rb
@@ -0,0 +1,18 @@
+class label
+    attr_accessor :title, :color
+    attr_reader :id, :items
+
+
+    def initialize(params)
+        @id = rand(1..10_000)
+        @title = params[:title]
+        @color = params[:color]
+        @items = []
+    end
+
+    def add_item(item)
+        @items << item unless @items.include?(item)
+        item.add_label(self)
+    end
+end
+
