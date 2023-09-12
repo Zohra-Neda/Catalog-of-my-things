888cc731e8181ddf9738d804a28bf369c2b54117
olanike olatunji <olanikegloria2020@gmail.com>
2023-09-12 20:00:09 +0100

    created musicAlbum.rb file

---
+++ music/musicAlbum.rb
@@ -0,0 +1,18 @@
+require_relative 'item'
+
+class Book < Item
+  attr_accessor  :on_spotify
+
+  def initialize(params)
+    super(params)
+    @on_spotify = params[:on_spotify]
+  end
+
+  private
+
+  def can_be_archived?
+    return true if super || @on_spotify == true
+
+    false
+  end
+end
