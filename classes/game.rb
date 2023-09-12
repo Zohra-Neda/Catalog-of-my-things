13948039a4f1513986488114788dc2a6665f9c7f
yemidada <yemidada@gmail.com>
2023-09-12 22:43:56 +0100

    Update game file

--- classes/game.rb
+++ classes/game.rb
@@ -12,11 +12,12 @@ class Game < Item
 
   def create_date(date_string)
     return nil if date_string.nil?
-    
+
     Date.parse(date_string.to_s)
   end
 
   def can_be_archived?
     super && (Date.today - @played_at_date).to_i / 365 > 2
   end
+  
 end
