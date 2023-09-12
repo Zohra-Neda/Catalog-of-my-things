3e705248651a9bd690189c86a544b649fc3ee7fc
yemidada <yemidada@gmail.com>
2023-09-12 23:04:09 +0100

    Update item file

--- classes/item.rb
+++ classes/item.rb
@@ -2,12 +2,12 @@ class Item
   attr_accessor :id, :genre, :author, :label, :publish_date, :archived
 
   def initialize(params)
-    @id = params[:id] || rand(1..10_000)
-    @genre = params[:genre]
-    @author = params[:author]
-    @label = params[:label]
-    @publish_date = create_date(params[:publish_date])
-    @archived = params[:archived] || false
+    @id = params['id'] || rand(1..10_000)
+    @genre = params['genre']
+    @author = params['author']
+    @label = params['label']
+    @publish_date = create_date(params['publish_date'])
+    @archived = params['archived'] || false
   end
 
   def add_genre(genre)
