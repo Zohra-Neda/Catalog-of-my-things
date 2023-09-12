f9d6a4d3c5c03d6a0290c3a9cb92cdc662408bf3
yemidada <yemidada@gmail.com>
2023-09-12 10:32:06 +0100

    Fix linter error

--- classes/game.rb
+++ classes/game.rb
@@ -1,13 +1,13 @@
 require_relative './item'
 
-class Game <  Item
+class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
   def initialize(params)
     super(params)
 
-    @multiplayer = params["#{:multiplayer}"]
-    @last_played_at = params["#{:last_played_at}"]
+    @multiplayer = params[:multiplayer]
+    @last_played_at = params[:last_played_at]
   end
 
   def create_date(date_string)
