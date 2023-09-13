51392de64353c023126523f4d95fedd355584154
zohra-neda <zohra.neda@gmail.com>
2023-09-13 14:04:40 +0430

    Fix linter errors

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,32 +1,30 @@
-require_relative '../classes/item'
 require_relative '../classes/book'
 
-RSpec.describe Book do
-  let(:book_params) do
+describe Book do
+  let(:base_params) do
     {
-      id: 123,
-      genre: 'Mystery',
-      author: 'Agatha Christie',
-      label: 'HarperCollins',
-      publish_date: Date.new(1926, 1, 21),
+      id: 1,
+      genre: 'Fiction',
+      author: 'John Doe',
+      label: 'Bestseller Books',
+      publish_date: '2010-01-01',
       archived: false,
-      publisher: 'William Collins, Sons',
+      publisher: 'Book Publisher',
       cover_state: 'good'
     }
   end
 
-  describe '#initialize' do
-    it 'creates a book object' do
-      book = Book.new(book_params)
+  subject(:book) { described_class.new(base_params) }
 
-      expect(book).to be_an_instance_of(Book)
-      expect(book.id).to eq(123)
-      expect(book.genre).to eq('Mystery')
-      expect(book.author).to eq('Agatha Christie')
-      expect(book.label).to eq('HarperCollins')
-      expect(book.publish_date).to eq(Date.new(1926, 1, 21))
+  describe '#initialize' do
+    it 'initializes a Book object with the given parameters' do
+      expect(book.id).to eq(1)
+      expect(book.genre).to eq('Fiction')
+      expect(book.author).to eq('John Doe')
+      expect(book.label).to eq('Bestseller Books')
+      expect(book.publish_date).to eq(Date.parse('2010-01-01'))
       expect(book.archived).to be_falsey
-      expect(book.publisher).to eq('William Collins, Sons')
+      expect(book.publisher).to eq('Book Publisher')
       expect(book.cover_state).to eq('good')
     end
   end
@@ -40,47 +38,18 @@ RSpec.describe Book do
       end
     end
 
-    context 'when the book is less than 10 years old' do
-      it 'returns false' do
-        book_params[:publish_date] = Date.today
-
-        book = Book.new(book_params)
-
-        expect(book.can_be_archived?).to be_falsey
-      end
-    end
-
-    context 'when the book cover state is bad' do
-      it 'returns true' do
-        book_params[:cover_state] = 'bad'
-
-        book = Book.new(book_params)
-
-        expect(book.can_be_archived?).to be_truthy
-      end
-    end
-  end
-
-  describe '#move_to_archive' do
-    context 'when the book can be archived' do
-      it 'sets archived to true' do
-        book = Book.new(book_params)
-
+    context 'when cover state is good and publish date is over 10 years ago' do
+      it 'archives the book' do
         book.move_to_archive
-
-        expect(book.archived).to be_truthy
+        expect(book.send(:can_be_archived?)).to be_truthy
       end
     end
 
-    context 'when the book cannot be archived' do
-      it "doesn't change the archived attribute" do
-        book_params[:publish_date] = Date.today
-
-        book = Book.new(book_params)
-
+    context 'when cover state is good but publish date is recent' do
+      it 'does not archive the book' do
+        book.publish_date = Date.today
         book.move_to_archive
-
-        expect(book.archived).to be_falsey
+        expect(book.send(:can_be_archived?)).to be_falsey
       end
     end
   end
