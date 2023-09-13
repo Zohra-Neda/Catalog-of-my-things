4d185eb98bfeb335d660636537bd69f70b4f3ee0
zohra-neda <zohra.neda@gmail.com>
2023-09-13 15:15:53 +0430

    Fix linter errors

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,28 +1,54 @@
 require_relative '../classes/game'
+require_relative '../classes/author'
 require 'date'
 
-describe Game do
-  arr = {
-    'multiplayer' => 'John',
-    'archived' => true,
-    'last_played_at' => '2022-01-01',
-    'publish_date' => '2022-01-01'
-  }
+RSpec.describe Game do
+  let(:author) { Author.new(first_name: 'John', last_name: 'Doe') }
+  let(:game) { Game.new(multiplayer: true, played_at_date: Date.today) }
 
-  context 'When you create a instance of the game' do
-    it 'has a multiplayer, archived and last_played_at' do
-      game = Game.new(arr)
+  it "associates with an author and adds the game to author's items" do
+    game = Game.new({ multiplayer: true, played_at_date: Date.today })
+    author.add_item(game)
 
-      expect(game.multiplayer).to eq('John')
-      expect(game.last_played_at).to eq(Date.parse('2022-01-01'))
-    end
+    expect(author.items).to include(game)
+    expect(game.author).to eq(author)
   end
 
-  context 'when you want check can_be_archived' do
-    it 'can check can_be_archived' do
-      game = Game.new(arr)
+  it 'cannot be archived if it doesn\'t meet the conditions' do
+    allow(Date).to receive(:today).and_return(Date.new(2023, 8, 10))
 
-      expect(game.can_be_archived?).to eq(false)
-    end
+    game = Game.new({
+                      multiplayer: true,
+                      played_at_date: Date.new(2022, 1, 1),
+                      publish_date: Date.new(2022, 1, 1) # Use Date object here
+                    })
+
+    game.move_to_archive
+
+    expect(game.archived).to be(false)
+  end
+  it 'can be archived if it meets the conditions' do
+    allow(Date).to receive(:today).and_return(Date.new(2023, 8, 10))
+    game = Game.new({
+                      multiplayer: true,
+                      played_at_date: Date.new(2022, 1, 1),
+                      publish_date: Date.new(2019, 1, 1)
+                    })
+    allow(game).to receive(:can_be_archived?).and_return(true)
+    game.move_to_archive
+    expect(game.archived).to be(true)
+  end
+
+  it 'returns a hash representation of the game' do
+    game = Game.new({ multiplayer: true, publish_date: Date.parse('2020-01-01'),
+                      played_at_date: Date.parse('2021-01-01') })
+
+    expected_hash = {
+      multiplayer: true,
+      publish_date: Date.parse('2020-01-01'),
+      played_at_date: Date.parse('2021-01-01')
+    }
+
+    expect(game.to_hash).to eq(expected_hash)
   end
 end
